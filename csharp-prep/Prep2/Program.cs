f0d3114f9e4c64a4bdddf96e5a28e8f66cd4b561
Weston Hyer <westonhyer@gmail.com>
2026-04-25 20:47:56 -0600

    Completed core requirements.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        
+        string letter = "";
+
+        if (grade > 90)
+        {
+             letter = "A";
+        }
+
+        else if (grade > 80 && grade < 90)
+        {
+             letter = "B";
+        }
+
+        else if (grade > 70 && grade < 80)
+        {
+             letter = "C";
+        }
+
+        else if (grade > 60 && grade < 70)
+        {
+             letter = "D";
+        }
+
+        else
+        {
+             letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You have passed this class. Great work!");
+        }
+
+        else
+        {
+            Console.WriteLine("You have not passed this class. Try again next time!");
+        }
     }
 }
