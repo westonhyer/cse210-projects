ef39dfc6aa51e9dc00833c3f3fbc206916466bbe
Weston Hyer <westonhyer@gmail.com>
2026-04-29 22:42:01 -0600

    Completed core requirements.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    Console.WriteLine("Welcome to the number guessing game!");
+    Random randomGenerator = new Random();
+    int magicNumber = randomGenerator.Next(1,100);
+    int guess = 100000;
+    string userInput = "";
+    int guessNumber = 0;
+
+    while (guess != magicNumber)
+        {
+            Console.WriteLine("Guess a number between 1 and 100");
+            userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher.");
+            }
+
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower.");
+            }
+            guessNumber += 1;
+        }
+    Console.WriteLine($"You guessed it in {guessNumber} guesses!");
+
     }
 }
