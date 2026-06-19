b5bd7c090d34ea96e52daadb3753b2549897ebbf
Weston Hyer <westonhyer@gmail.com>
2026-06-18 22:22:39 -0600

    Completed learning activity.

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(double radius, string color) : base (color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * (_radius * _radius);
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Program started");
+        
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square(25, "Blue");
+        Circle c1 = new Circle(5, "Green");
+        Rectangle r1 = new Rectangle(4, 6, "Red");
+
+        shapes.Add(s1);
+        shapes.Add(c1);
+        shapes.Add(r1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
